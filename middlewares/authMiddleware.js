1595cb0629d8eceba602c7f07bd95108a9176ccb
Andres Antonio Segovia Ampuero <149842009+Oerlinker@users.noreply.github.com>
2024-10-09 15:07:26 -0400

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = (roleRequired) => (req, res, next) => {
     const token = req.header('Authorization');
-
     if (!token) {
-        return res.status(401).json({ message: 'Acceso denegado' });
+        return res.status(401).json({ message: 'Acceso denegado, no hay token' });
     }
-const tokenPart=token.split(' ')[1];
     try {
         const verified = jwt.verify(token, 'secretKey');
         req.user = verified;
+
+        if (req.user.rol !== roleRequired) {
+            return res.status(403).json({ message: 'Acceso denegado, no tienes el rol adecuado' });
+        }
+
         next();
     } catch (error) {
-        res.status(401).json({ message: 'Token inválido' });
+        res.status(400).json({ message: 'Token inválido' });
     }
 };
 
